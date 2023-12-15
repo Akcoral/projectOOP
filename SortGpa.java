d59d8defcf30fe920c84a24eff1b7e382bb7562c
Akcoral <123075685+Akcoral@users.noreply.github.com>
2023-12-15 11:47:56 +0600

    Create SortGpa.java

---
+++ SortGpa.java
@@ -0,0 +1,11 @@
+package projectFinal;
+
+	import java.util.Comparator;
+
+	public class SortGpa implements Comparator<User>{
+		public int compare(User u1, User u2) {
+			Student s1 = (Student)u1;
+			Student s2 = (Student)u2;
+			return s1.compareTo(s2);
+		}
+	}
